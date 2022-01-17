e5c88c12ca0fdc84a9b317faa490c3a03c92bc5f
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-01-17 11:23:48 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pi;
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+
+
+int main()
+{
+//freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int T; T=1; cin >> T;
+    while(T--)
+    {
+
+    }
+}
+
