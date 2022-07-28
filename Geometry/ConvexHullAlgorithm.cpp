de7bcf1f5a7d16b53f04f4d7a5a514feff23b9b1
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-07-28 17:00:21 +0600

    Update ConvexHullAlgorithm.cpp

--- Geometry/ConvexHullAlgorithm.cpp
+++ Geometry/ConvexHullAlgorithm.cpp
@@ -1,84 +1,43 @@
-struct point{
-int x, y;
+struct pt {
+    double x, y;
 };
 
-struct vect{
-int a, b;
-};
-
-point pinitial;
-
-int crossProduct(vect v1, vect v2)
-{
-    return ((v1.a*v2.b)-(v1.b*v2.a));
+int orientation(pt a, pt b, pt c) {
+    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
+    if (v < 0) return -1; // clockwise
+    if (v > 0) return +1; // counter-clockwise
+    return 0;
 }
 
-int direction(point pi, point pj, point pk)
-{
-    vect v1, v2;
-    v1.a=pk.x-pi.x;
-    v1.b=pk.y-pi.y;
-
-    v2.a=pj.x-pi.x;
-    v2.b=pj.y-pi.x;
-    //cout << crossProduct(v1, v2) << endl;
-    return crossProduct(v1, v2);
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
 }
+bool collinear(pt a, pt b, pt c) { return orientation(a, b, c) == 0; }
 
-bool cmpr(point p1, point p2)
-{
-    if(direction(pinitial, p2, p1)>0)
-    {
-        return 1;
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
+                < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+        return o < 0;
+    });
+    if (include_collinear) {
+        int i = (int)a.size()-1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin()+i+1, a.end());
     }
 
-    else
-    {
-        return 0;
-    }
-}
-
-int main()
-{
-    int minix=1000000000;
-    int miniy=1000000000;
-    int mini=-1;
-    for(int i=0;i<n;i++)
-    {
-        if(p[i].y<miniy || (p[i].y==miniy && p[i].x<minix))
-        {
-            mini=i;
-            minix=p[i].x;
-            miniy=p[i].y;
-        }
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        st.push_back(a[i]);
     }
 
-    point temp;
-    temp=p[0];
-    p[0]=p[mini];
-    p[mini]=temp;
-
-    pinitial=p[0];
-    sort(p+1, p+n, cmpr);
-
-
-    S.push_back(p[0]);
-    S.push_back(p[1]);
-    S.push_back(p[2]);
-
-    for(int i=3;i<n;i++)
-    {
-        point top=S[S.size()-1];
-        point ntotop=S[S.size()-2];
-        int d=direction(top, ntotop, p[i]);
-        //cout << "Top " << top.x << " " << top.y << endl;
-        //cout << "NTOTOP " << ntotop.x << " " << ntotop.y << endl;
-        //cout << "New " << p[i].x << " " << p[i].y << endl;
-        //cout << d << endl;
-        if(d<0)
-        {
-            S.pop_back();
-        }
-        S.push_back(p[i]);
-    }
+    a = st;
 }
