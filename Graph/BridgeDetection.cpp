f574a50550ae4f95be9592a8466b76c18f49dfd3
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2023-04-12 16:46:12 +0600

    Update BridgeDetection.cpp

--- Graph/BridgeDetection.cpp
+++ Graph/BridgeDetection.cpp
@@ -1,1 +1,10 @@
-//abc
+int vis[mx], low[mx], tin[mx], timer=0;
+void dfs(ll u, ll p=-1) { vis[u]=1;
+    low[u]=tin[u]=++timer;
+    for(ll v:g[u]) { if(v==p) continue;
+        if(!vis[v]) { dfs(v, u); low[u]-min(low[u], low[v]); }
+        else { low[u]=min(low[u], tin[v]); }
+        if(low[v]>tin[v]) { // isbridge(u, v);
+        }
+    }
+}
